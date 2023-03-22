35b36669f2a2ce5b82dfa213476ebe583b2fbf34
shishir610 <shishir.srik@gmail.com>
2023-03-22 16:28:40 -0700

    Implemented OAuth on frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react"
+import { GoogleOAuthProvider, GoogleLogin } from "@react-oauth/google"
 import "./App.css"
 
 const App = () => {
@@ -18,27 +19,44 @@ const App = () => {
     }
 
     return (
-        <div className="App">
-            <div>Choose a project</div>
-            {projects.map((project) => {
-                return (
-                    <button
-                        onClick={() => {
-                            console.log(project)
-                            handleProjectButton(project)
-                        }}
-                    >
-                        {project}
-                    </button>
-                )
-            })}
-            <div>
-                {project ? (
-                    <img src={`http://localhost:5000/static/${project}.png`} />
-                ) : (
-                    ""
-                )}
-            </div>
+        <div className="m-4 d-flex justify-content-center">
+            <GoogleOAuthProvider clientId={process.env.REACT_APP_CLIENT_ID}>
+                <div className="App">
+                    <div className="mb-2">
+                        <GoogleLogin
+                            onSuccess={(credentialResponse) => {
+                                console.log(credentialResponse)
+                            }}
+                            onError={() => {
+                                console.log("Login Failed")
+                            }}
+                        />
+                    </div>
+                    <div className="mb-2">Choose a project</div>
+                    {projects.map((project) => {
+                        return (
+                            <button
+                                key={project}
+                                onClick={() => {
+                                    console.log(project)
+                                    handleProjectButton(project)
+                                }}
+                            >
+                                {project}
+                            </button>
+                        )
+                    })}
+                    <div>
+                        {project ? (
+                            <img
+                                src={`http://localhost:5000/static/${project}.png`}
+                            />
+                        ) : (
+                            ""
+                        )}
+                    </div>
+                </div>
+            </GoogleOAuthProvider>
         </div>
     )
 }
