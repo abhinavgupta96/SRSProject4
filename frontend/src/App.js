ba20c5ecd7f7dae30ff745141e5fee665f285ce0
shishir610 <shishir.srik@gmail.com>
2023-03-24 16:50:51 -0700

    Added frontend functionality for gantt chart and developer performance charts

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,60 +1,105 @@
-import React, { useEffect, useState } from "react"
+import React, { useEffect, useRef, useState } from "react"
 import { GoogleOAuthProvider, GoogleLogin } from "@react-oauth/google"
 import "./App.css"
+import ButtonList from "./components/ButtonList"
+
+const IMG_BASE = "http://localhost:5000/static"
 
 const App = () => {
-    const [projects, setProjects] = useState([])
-    const [project, setProject] = useState(null)
+    const [charts, setCharts] = useState([])
+    const [developers, setDevelopers] = useState([])
+    const [chart, setChart] = useState(null)
+    const imgRef = useRef()
 
     useEffect(() => {
         fetch("/sprint_burndown").then((response) => {
+            console.log(response)
             response.json().then((data) => {
-                setProjects(data["Project Codes"])
+                console.log(data["Project Codes"])
+                setCharts((charts) => [...charts, ...data["Project Codes"]])
             })
         })
     }, [])
 
-    const handleProjectButton = (project) => {
-        setProject(project)
+    useEffect(() => {
+        fetch("/gantt_chart").then((response) => {
+            response.json().then((data) => {
+                console.log("HELLO")
+                if (data[201] == "Done") {
+                    setCharts((charts) => [...charts, "gantt_chart"])
+                }
+            })
+        })
+    }, [])
+
+    useEffect(() => {
+        fetch("/get_developers").then((response) => {
+            response.json().then((data) => {
+                if (data["Developers"]) {
+                    setDevelopers(data["Developers"])
+                }
+            })
+        })
+    }, [])
+
+    const handleChartDisplay = (chart, type) => {
+        let img = imgRef.current
+        img.style.height = "400px"
+        img.style.width = "auto"
+        switch (type) {
+            case "PROJECT": {
+                setChart(chart)
+                return
+            }
+            case "DEVELOPER": {
+                setChart(`Performance_${chart}`)
+                return
+            }
+        }
     }
 
     return (
-        <div className="m-4 d-flex justify-content-center">
+        <div className="m-4 d-flex flex-column justify-content-center">
             <GoogleOAuthProvider clientId={process.env.REACT_APP_CLIENT_ID}>
-                <div className="App">
-                    <div className="mb-2">
-                        <GoogleLogin
-                            onSuccess={(credentialResponse) => {
-                                console.log(credentialResponse)
-                            }}
-                            onError={() => {
-                                console.log("Login Failed")
-                            }}
-                        />
+                <div className="">
+                    <p className="fw-bold">
+                        <mark>
+                            <bold>Agile Chart Visualization</bold>
+                        </mark>
+                    </p>
+                    <hr />
+                    <div className="m-2">
+                        <p>
+                            <mark>Choose a chart</mark>
+                        </p>
                     </div>
-                    <div className="mb-2">Choose a project</div>
-                    {projects.map((project) => {
-                        return (
-                            <button
-                                key={project}
-                                onClick={() => {
-                                    console.log(project)
-                                    handleProjectButton(project)
-                                }}
-                            >
-                                {project}
-                            </button>
-                        )
-                    })}
-                    <div>
-                        {project ? (
-                            <img
-                                src={`http://localhost:5000/static/${project}.png`}
-                            />
-                        ) : (
-                            ""
-                        )}
+                    <ButtonList
+                        buttons={charts}
+                        handleChartDisplay={handleChartDisplay}
+                        type="PROJECT"
+                    />
+                    <hr />
+                    <div className="m-2">
+                        <p>
+                            <mark>
+                                Choose a developer to view their performance
+                                chart
+                            </mark>
+                        </p>
                     </div>
+                    <ButtonList
+                        buttons={developers}
+                        handleChartDisplay={handleChartDisplay}
+                        type="DEVELOPER"
+                    />
+                </div>
+                <div>
+                    <img
+                        ref={imgRef}
+                        src={`${IMG_BASE}/${chart}.png`}
+                        width={"0px"}
+                        height={"0px"}
+                    />
                 </div>
             </GoogleOAuthProvider>
         </div>
---
+++ frontend/src/components/ButtonList.js
@@ -0,0 +1,24 @@
+import React from "react"
+
+const ButtonList = ({ buttons, handleChartDisplay, type }) => {
+    return (
+        <React.Fragment>
+            {buttons.map((button) => {
+                return (
+                    <button
+                        key={button}
+                        style={{ maxWidth: "auto", margin: "0 5px" }}
+                        onClick={() => {
+                            handleChartDisplay(button, type)
+                        }}
+                        className="btn btn-info"
+                    >
+                        {button}
+                    </button>
+                )
+            })}
+        </React.Fragment>
+    )
+}
+
+export default ButtonList
