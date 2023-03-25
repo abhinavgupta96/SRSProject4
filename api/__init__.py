dafe0e9f34385decdb96eada30344e9998141f91
shishir610 <shishir.srik@gmail.com>
2023-03-25 11:24:59 -0700

    Added POST routes /register, /login

--- api/__init__.py
+++ api/__init__.py
@@ -1,11 +1,14 @@
 from flask import Flask
-from flask import Blueprint, jsonify, request
+from route import main
+from flask_cors import CORS
+
 
 def create_app():
     app = Flask(__name__)
-    from route import main
+    CORS(app)
     app.register_blueprint(main)
     return app
+
 
 if __name__ == "__main__":
     app = create_app()
--- api/route.py
+++ api/route.py
@@ -1,9 +1,11 @@
-from flask import Blueprint, jsonify, request
+from flask import Blueprint, jsonify, make_response, request
 from datetime import date, timedelta
 import pandas as pd
 import matplotlib.pyplot as plt
 import os
 import glob
+from flask import Flask, jsonify, request
+from werkzeug.security import generate_password_hash, check_password_hash
 
 main = Blueprint('main', __name__)
 
@@ -12,6 +14,55 @@ def createResponseObject(obj):
     response = jsonify(obj)
     response.headers.add('Access-Control-Allow-Origin', '*')
     return response
+
+
+# Mimic users table in DB as in-memory array for prototype
+users = []
+
+
+@main.route('/register', methods=['POST'])
+def register():
+    print("GOT A REQUEST")
+    data = request.get_json()
+    username = data.get('username')
+    password = data.get('password')
+
+    if not username or not password:
+        return jsonify({'message': 'Either/both username or password is missing'}), 400
+
+    if len(username) < 4 or len(username) > 32:
+        return jsonify({'message': 'Username does not conform to length restrictions'}), 400
+
+    if len(password) < 8 or len(password) > 20:
+        return jsonify({'message': 'Password does not conform to length restrictions'}), 400
+
+    for user in users:
+        if user['username'] == username:
+            return jsonify({'message': 'User already exists'}), 409
+
+    passwordHash = generate_password_hash(password, method='sha256')
+
+    user = {'username': username, 'password': passwordHash}
+    users.append(user)
+
+    return jsonify({'message': 'User was registered added to DB'}), 201
+
+
+@main.route('/login', methods=['POST'])
+def login():
+    data = request.get_json()
+    username = data.get('username')
+    password = data.get('password')
+
+    if not username or not password:
+        return jsonify({'message': 'Either/both username or password is missing'}), 400
+
+    for user in users:
+        if user['username'] == username:
+            if check_password_hash(user['password'], password):
+                return jsonify({'message': 'Login was successful'}), 200
+
+    return jsonify({'message': 'Invalid username or password. Please retry.'}), 401
 
 
 @main.route('/sprint_burndown', methods=['GET'])
@@ -70,8 +121,9 @@ def sprint_burndown_project_name(project_name):
         story_completed[i] = df2
         story_planned[i] = df4
     create_figure(story_completed, story_planned, project_name)
-    status_code = {201 : "Done"}
+    status_code = {201: "Done"}
     return createResponseObject(status_code)
+
 
 @main.route('/gantt_chart', methods=['GET'])
 def gantt_chart():
@@ -85,12 +137,15 @@ def gantt_chart():
     for project in project_code:
         df_project = df_planning.loc[df_planning['Project'] == project]
         sprint_value = df_project.Sprint.unique()
-        start_date = df_project.loc[df_project['Sprint'] == sprint_value[0]].StartDate.iloc[0].strftime('%Y-%m-%d')
-        complete_date = df_project.loc[df_project['Sprint'] == sprint_value[-1]].CompleteDate.iloc[0].strftime('%Y-%m-%d')
-        schedule_info[project] = [start_date,complete_date]
+        start_date = df_project.loc[df_project['Sprint'] ==
+                                    sprint_value[0]].StartDate.iloc[0].strftime('%Y-%m-%d')
+        complete_date = df_project.loc[df_project['Sprint'] ==
+                                       sprint_value[-1]].CompleteDate.iloc[0].strftime('%Y-%m-%d')
+        schedule_info[project] = [start_date, complete_date]
     create_chart(schedule_info)
-    status_code = {201 : "Done"}
+    status_code = {201: "Done"}
     return createResponseObject(status_code)
+
 
 def create_chart(schedule_info):
     file_name = 'gantt_chart.png'
@@ -106,30 +161,36 @@ def create_chart(schedule_info):
         end_date = schedule_info[project][1]
         start.append(start_date)
         end.append(end_date)
-    df = pd.DataFrame(data={"Project" : project_code, "Start": start, "End": end})
+    df = pd.DataFrame(data={"Project": project_code,
+                      "Start": start, "End": end})
     df["Start"] = pd.to_datetime(df.Start)
     df["End"] = pd.to_datetime(df.End)
     df["Days"] = df["End"] - df["Start"]
     df["Color"] = plt.cm.Set1.colors[:len(df)]
     plt.style.use("ggplot")
     plt.switch_backend('Agg')
-    fig = plt.figure(figsize=(20,10))
-    plt.barh(y=df["Project"], left=df["Start"], width=df["Days"], color=df["Color"])
+    fig = plt.figure(figsize=(20, 10))
+    plt.barh(y=df["Project"], left=df["Start"],
+             width=df["Days"], color=df["Color"])
     current_date = date.today()
     text_date = current_date + timedelta(days=5)
-    plt.vlines(x=current_date, ymin=-0.5, ymax=5, colors="dodgerblue", linestyles="dashed", linewidth=5)
-    plt.text(x=text_date, y=4, s="Today", fontsize=20, fontweight="bold", color="black")
-    plt.xlim(date(2023,1,1), date(2023,12,31))
+    plt.vlines(x=current_date, ymin=-0.5, ymax=5,
+               colors="dodgerblue", linestyles="dashed", linewidth=5)
+    plt.text(x=text_date, y=4, s="Today", fontsize=20,
+             fontweight="bold", color="black")
+    plt.xlim(date(2023, 1, 1), date(2023, 12, 31))
     plt.ylim(-0.5, 5)
     dt_rng = pd.date_range(start="2023-1-1", end="2023-12-31", freq="MS")
-    plt.xticks(dt_rng,[dt.month_name() for dt in dt_rng], fontsize=15)
+    plt.xticks(dt_rng, [dt.month_name() for dt in dt_rng], fontsize=15)
     plt.yticks(fontsize=15)
     plt.xlabel("Date", fontsize=20, fontweight="bold", labelpad=10)
     plt.ylabel("Project", fontsize=20, fontweight="bold", labelpad=10)
-    plt.title("Gantt Chart", fontsize=30, loc="center", pad=20, fontweight="bold")
+    plt.title("Gantt Chart", fontsize=30,
+              loc="center", pad=20, fontweight="bold")
     plt.plot()
     plt.savefig(file_path)
     plt.close()
+
 
 @main.route('/get_developers', methods=['GET'])
 def get_developers():
@@ -139,10 +200,11 @@ def get_developers():
     file = file_list[0]
     df = pd.read_excel(file, sheet_name="SprintData")
     list_developers = df.Developer.unique()
-    dev = {"Developers" : []}
+    dev = {"Developers": []}
     for developer in list_developers:
-        dev["Developers"].append(developer) 
+        dev["Developers"].append(developer)
     return createResponseObject(dev)
+
 
 @main.route('/developer_performance/<developer_name>', methods=['GET'])
 def developer_performance(developer_name):
@@ -162,11 +224,12 @@ def developer_performance(developer_name):
             sum = df_project["Story Point"].sum()
             project_perf[project] = sum
         dev_perf[developer] = project_perf
-    create_perf_chart(dev_perf,developer_name)
-    status_code = {201 : "Done"}
+    create_perf_chart(dev_perf, developer_name)
+    status_code = {201: "Done"}
     return createResponseObject(status_code)
 
-def create_perf_chart(dev_perf,developer_name):
+
+def create_perf_chart(dev_perf, developer_name):
     figure_title = "Performance Chart for " + developer_name
     file_name = 'Performance_' + developer_name + '.png'
     figure_path = os.path.join(os.path.abspath(
@@ -178,17 +241,22 @@ def create_perf_chart(dev_perf,developer_name):
         project_code.append(project)
     for point in perf_chart.values():
         story_point.append(point)
-    df = pd.DataFrame(data={"Project" : project_code, "StoryPoint": story_point})
+    df = pd.DataFrame(data={"Project": project_code,
+                      "StoryPoint": story_point})
     plt.style.use("ggplot")
     plt.switch_backend('Agg')
-    fig = plt.figure(figsize=(20,10))
+    fig = plt.figure(figsize=(20, 10))
     x_axis = project_code
     y_axis = story_point
     plt.xticks(fontsize=15)
     plt.yticks(fontsize=15)
-    plt.plot(x_axis, y_axis, color='blue', label='Completed Points', linewidth=4, linestyle="dashdot")
-    plt.xlabel("Project Codes", fontsize=20, fontweight="bold", labelpad=10, color="black")
-    plt.ylabel("Story Points", fontsize=20, fontweight="bold", labelpad=10, color="black")
-    plt.ylim(0,40)
-    plt.title(figure_title, fontsize=30, loc="center", pad=20, fontweight="bold")
+    plt.plot(x_axis, y_axis, color='blue', label='Completed Points',
+             linewidth=4, linestyle="dashdot")
+    plt.xlabel("Project Codes", fontsize=20,
+               fontweight="bold", labelpad=10, color="black")
+    plt.ylabel("Story Points", fontsize=20,
+               fontweight="bold", labelpad=10, color="black")
+    plt.ylim(0, 40)
+    plt.title(figure_title, fontsize=30,
+              loc="center", pad=20, fontweight="bold")
     plt.savefig(figure_path)
